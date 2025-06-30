69b354c538e3a9042bf510efe9ec0a2bc1076ff0
Steve <steve@Steves-MacBook-Pro.local>
2025-06-30 19:41:30 -0400

    First commit

--- content.js
+++ content.js
@@ -9,6 +9,7 @@ class AmazonBrandTracker {
 
 
     this.pageParser = new TypicalProductPageParser();
+    this.bookPageParser= new BookPageParser();
     this.networkManager = new NetworkManager();
     
     this.init();
@@ -25,7 +26,7 @@ class AmazonBrandTracker {
     }
 
     // This is init here to correclty load the user toggled setting (if set).
-    this.displayElementManager = new DisplayElementManager(this.layoutMode)
+    this.displayElementManager = new  CypherDisplayElementManager(this.layoutMode)
 
     // Wait for page to load and then extract brand info
     if (document.readyState === 'loading') {
@@ -36,68 +37,92 @@ class AmazonBrandTracker {
   }
 
  
+  classifyProductAndExtractBrandInfo() {
+    // Check for manufacturer information first (more reliable for ownership).
+      // If this is found directly on the page we are done and can update the UI component.
+      console.log('Starting manufacturer info extraction...');
+      let brandInfo = this.pageParser.extractManufacturerInfo();
+      if (brandInfo != null && brandInfo.manufacturer !== 'information...' ) {
+        // Initial success case the manufacture field exists on the product page.
+        console.log('Found manufacturer info directly from page:', brandInfo);
+      } else {
+        // If the manufauring company field is not explicitly listed we need to extract the brand instead.
+        // If no brand is listed this will be an error state and the UI will be updated accordingly.
+        console.log('Starting brand info extraction...');
+        brandInfo = this.pageParser.extractBrandName();
+        if (brandInfo) {
+          const displayInfo2 = brandInfo || 'no-info-found';
+          console.log('Found brand/book info:', displayInfo2);
+        } else {
+          // If the brand is not listed this could be a book page.
+          // Book pages will have their own category specific text on the UI component.
+          console.log('Processing book page...');
+          brandInfo = this.bookPageParser.extractBrandName();
+          // TODO: Handle other corner cases. 
+          // There are many types of Amazon product pages so there will likely be many more.     
+        }
+      }
+      return brandInfo
+  }
+
   /* This method ties together the methods from the other modules:
      DisplayElementFactory, PageParser, and NetworkManager.
   */
-  async classifyWebpageExtractInfoAndUpdateDisplay() {
+  async classifyWebpageExtractInfoAndUpdateDisplay () {
 
-        // First we create the component. It will intially be in its laoding state.
-        console.log('Now displaying extension component...');
-        const displayInfo = {'type': 'product_with_manufacturer'}
-        const loadingElement = this.displayElementManager.createDisplayElement(displayInfo, null, true);
-        loadingElement.classList.add('loading');
-        this.displayElementManager.insertDisplayElement(loadingElement);
-        
-        
-        // Check for manufacturer information first (more reliable for ownership).
-        // If this is found directly on the page we are done and can update the UI component.
-        console.log('Starting manufacturer info extraction...');
-        const manufacturerInfo = this.pageParser.extractManufacturerInfo();
-        if (manufacturerInfo != null) {
-            console.log('Found manufacturer info directly from page:', manufacturerInfo);
-            setTimeout(() => {
-                this.displayElementManager.updateDisplayElement(manufacturerInfo, null);
-            }, 500);
-            return
-        }
-
-        // If the manufauring company is not explicitly listed we need to extract the brand instead.
-        // If no brand is listed this will be an error state and the UI will be updated accordingly.
-        console.log('Starting brand info extraction...');
-        const brandInfo = this.pageParser.extractBrandName();
-        const displayInfo2 = brandInfo || 'no-info-found';
-        console.log('Found brand/book info:', displayInfo2);
-        if (!brandInfo) {
-          console.log('No brand or book info found, showing error message...');
-          setTimeout(() => {
-              this.displayElementManager.updateDisplayElement('no-info-found', null);
-          }, 500);
-          return;
-        }
-        
-        // If the brand is not listed this could be a book page.
-        // Book pages will have their own category specific text on the UI component.
-        if (brandInfo && brandInfo.type === 'book') {
-          console.log('Processing book page...');
-          setTimeout(() => {
+      // First we create the component. It will intially be in its laoding state.
+      console.log('Now displaying extension component...');
+      const displayInfo = {'type': 'product_with_manufacturer'};
+      const loadingElement = this.displayElementManager.createDisplayElementWithComponent(displayInfo, null, true);
+      loadingElement.classList.add('loading');
+      this.displayElementManager.insertDisplayElement(loadingElement);
+      
+      // Classify the webpage parse it and return meta data describing what is in the product page.
+      const brandInfo = this.classifyProductAndExtractBrandInfo();
+  
+      // If found on Amazon product page of any type.
+      // When the company that owns the brand is found directly on the web page.
+      if (brandInfo) {
+        setTimeout(() => {
               this.displayElementManager.updateDisplayElement(brandInfo, null);
           }, 500);
+          return;
+      }
+
+      // Finally if all we got is the brand but no corresponding company behind it we call our own API to 
+      // match the brand with the company that owns it.
+      console.log('Processing regular product page...');
+      const ownerInfo = await this.networkManager.fetchBrandOwner(brandInfo);
+      console.log(`ownerInfo: ${ownerInfo}`);
+      console.log(`brand_name: ${ownerInfo.brand_name}`);
+      console.log(`owning_company_name: ${ownerInfo.owning_company_name}`);
+      
+      // Error case
+      if (!brandInfo) {
+        console.log('No brand info found, showing error message...');
+        setTimeout(() => {
+            this.displayElementManager.updateDisplayElement('no-info-found', null);
+        }, 500);
         return;
-        }
+      }
 
-        // TODO: Handle other corner cases. 
-        // There are many types of Amazon product pages so there will likely be many more.
-        
+      // Final case. Update with results of netowrk call.
+      // Update the UI compmenet with the newtork fetching company owner information.
+      this.displayElementManager.updateDisplayElement(brandInfo, ownerInfo); 
+  }
 
-        // Finally if all we got is the brand but no corresponding company behind it we call our own API to 
-        // match the brand with the company that owns it.
-        console.log('Processing regular product page...');
-        const ownerInfo = await this.networkManager.fetchBrandOwner(brandInfo);
+  async classifyWebpageExtractInfoAndUpdateDisplayWithCompassComponent() {
+      // Insert Compass component in loading state.
+      
+      // Classify the webpage parse it and return meta data describing what is in the product page.
+      // const brandInfo = this.classifyProductAndExtractBrandInfo();
+      // Optionally fetch company from network if needed
+      // const ownerInfo = await this.networkManager.fetchBrandOwner(brandInfo);
 
-        // Update the UI compmenet with the newtork fetching company owner information.
-        this.displayElementFactory.updateDisplayElement(brandInfo, ownerInfo);
-    }
+      // Call the Compass AI political leaning endpoint.
 
+      // Populate the Compass AI component with the correct info and take it our of loading state.
+  }
 }
 
 // Initialize the tracker
